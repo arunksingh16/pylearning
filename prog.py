2b0929becb0689571e8e0ebadfced6b869963f98
Arun Kumar Singh <arun161087@gmail.com>
2020-02-06 23:40:23 +0000

    first commit
    06 feb 2019

---
+++ prog.py
@@ -0,0 +1,18 @@
+x = input('Enter the number to add ')
+y = input('Enter the number to add ')
+
+
+def sum (c, d):
+    z = c + d
+    return z
+
+
+print(sum(x, y))
+
+
+def addition(a, b):
+    z = int(x) + int(y)
+    print(f'Sum of these two numbers: {z}')
+
+
+addition(x, y)
