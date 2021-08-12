d1c2da9f28edae463edc5736469ea7c2e8e22d42
Arun Kumar Singh <22137632+arunksingh16@users.noreply.github.com>
2021-08-12 17:15:24 +0100

    Create example.py

---
+++ format/example.py
@@ -0,0 +1,11 @@
+# The format() method formats the specified value(s) and insert them inside the string's placeholder.
+
+detailsHostname = "details" if (os.environ.get("DETAILS_HOSTNAME") is None) else os.environ.get("DETAILS_HOSTNAME")
+port = "port" if (os.environ.get("PORTS") is None) else os.environ.get("PORTS")
+
+details = {
+    "name": "http://{0}:{1}".format(detailsHostname, port),
+    "endpoint": "details",
+    "children": []
+}
+
